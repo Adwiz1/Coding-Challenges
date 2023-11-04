4d1b36375138a9e3979f3aff9acbb78f31e60f52
Adam Smith <adwiz1994@gmail.com>
2023-11-04 15:27:15 +0000

    Coding Challenge and solution for 04-11-23.

---
+++ CC November 2023/CC 2023-11-04/app.js
@@ -0,0 +1,24 @@
+// Challenge (8 kyu):
+// Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+
+// If no occurrences can be found, a count of 0 should be returned.
+
+// ("Hello", "o")  ==>  1
+// ("Hello", "l")  ==>  2
+// ("", "z")       ==>  0
+
+function strCount(str, letter){  
+    let letterCount = 0
+    let splitStr = str.split('')
+    for(let i = 0; i < str.length; i++){
+      if(splitStr[i] === letter){
+        letterCount += 1
+      }
+    }
+    return letterCount
+  }
+
+//Clever solution
+//   function strCount(str, letter){  
+//     return str.split(letter).length-1
+//   }
